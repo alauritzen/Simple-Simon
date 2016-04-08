5d4149cae601ba09d5b59422875273d31d537361
Alan <alaurit@gmail.com>
2016-04-08 16:00:06 -0500

    created start function, need to finish working on scorecard

--- public/js/simon_js.js
+++ public/js/simon_js.js
@@ -6,6 +6,8 @@
     // set variables for cpu string and user string
     var cpuString=[];
     var userString=[];
+    var current_score=0;
+    var high_score=0;
 
     // use jquery to get ids of buttons
     var $up = $("#box_up");
@@ -30,57 +32,82 @@
     // function to flash cpu string
     function flashSequence() {
         addCpuNumber();
+        // change round in scorecard
+        $("#current_round").text(cpuString.length); 
         
         var index = 0;
 
         var max = cpuString.length;
         var interval = 2000;
 
+
         var intervalId = setInterval(function() {
             if (index <= (max-1)) {
                 // cpuString[index].css("opacity", "1.0");
                 cpuString[index].animate({
                     opacity: "1.0"
-                }, 500);
+                }, 430);
                 cpuString[index].animate({
-                    opacity: "0.4"
-                }, 500);
+                    opacity: "0.6"
+                }, 430);
             
-                console.log("length "+ cpuString.length + ", current index: " + (index+1));
+                /*console.log("After running flash Sequence: cpu length "+ cpuString.length + ", cpu current index: " + index);
+                console.log("user input length "+ userString.length + ", user current index: " + index);*/
+
                 index++;
             }
         }, interval);
     } 
 
     // listener for user input
-    // if false, clear cpu string and user string
-    // if true, run flashSequence again
     buttons.forEach(function (whichButton,index, array) {
         whichButton.click(function() {
+            // buttons flash faster for user input
             whichButton.animate({
                 opacity: "1.0"
-            }, 500);
+            }, 200);
             whichButton.animate({
-                opacity: "0.4"
-            }, 500);
-            // compare input to cpu string
+                opacity: "0.6"
+            }, 200);
             userString.push(whichButton);
             var lastUserIndex = userString.length-1
+            // compare input to cpu string
+            // if false, clear cpu string and user string
             if (userString[lastUserIndex] != cpuString[lastUserIndex]) {
-                console.log(userString[0] + " user compare to cpu " + cpuString[0] + " not correctly")
+                console.log(userString[0] + " user compare to cpu " + cpuString[0] + " not correctly");
+                userString = [];
+                cpuString = [];
+                $("#start_text").text("Sorry. You've reached the end. Your score is " + current_score + ".").css("font-size", "20px");
+                console.log("length of cpu string: " + cpuString.length + "length of user string: " + userString.length);
+            // if true and complete string matches, clear user string for next round and run flashSequence again
+            } else if (userString.length == cpuString.length) {
+                console.log("Correct. Complete sequence matches. Prepare for next round")
+                userString = [];
+                flashSequence();
+            // otherwise input is correct, but string is not complete, so nothing happens yet
             } else {
                 console.log(userString[0] + " user compare to cpu " + cpuString[0] + " correctly");
             }
-            
         });
     });
 
+    // listener for start button runs flash sequence on start
+    $("#box_start").click(function() {
+        // scorecard slides down
+        $("#scorecard").slideDown();
+        userString = [];
+        cpuString = [];
+        flashSequence();
+        // start button text changes
+        $("#start_text").text("Good luck!")
+        console.log(cpuString);
+    });
 
-    addCpuNumber();
-    addCpuNumber();
-    addCpuNumber();
-    flashSequence();
-    console.log(cpuString);
+
+
+    
+    // flashSequence();
+    // console.log(cpuString);
     // console.log("length "+ cpuString.length + ", current count: " + count);
 
 
