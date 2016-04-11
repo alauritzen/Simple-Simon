80dbf6f2abab57c7e1d643331b416f3ec89ad969
Alan <alaurit@gmail.com>
2016-04-11 08:38:32 -0500

    weekend work

--- public/js/simon_js.js
+++ public/js/simon_js.js
@@ -10,12 +10,12 @@
     var high_score=0;
 
     // use jquery to get ids of buttons
-    var $up = $("#box_up");
-    var $left = $("#box_left");
-    var $down = $("#box_down");
-    var $right = $("#box_right");
+    var $red = $("#box_red");
+    var $blue = $("#box_blue");
+    var $orange = $("#box_orange");
+    var $green = $("#box_green");
 
-    var buttons = [$up, $left, $down, $right];
+    var buttons = [$red, $blue, $orange, $green];
 
     // random number function
     function getRandomInt() {
@@ -38,8 +38,14 @@
         }
     }
 
+    // turn off buttons listener with this function
+    // var turnOffInput = function(event) {
+    // event.preventDefault();
+    // }
+
     // function to flash cpu string
     function flashSequence() {
+        // whichButton.click(turnOffInput);
         addCpuNumber(); 
         
         var index = 0;
@@ -60,43 +66,61 @@
                 index++;
             }
         }, interval);
+        // userInput();
     } 
 
+    // function shiftColors() {
+        /*$("#box_red").animate({
+            left: "200px",
+            }, 430);*/
+
+        /*
+        $("#box_red").addClass("shift");
+        console.log("it ran");   
+    };
+
+
+    shiftColors(); */
+
     // listener for user input
-    buttons.forEach(function (whichButton,index, array) {
-        whichButton.click(function() {
-            // buttons flash faster for user input
-            whichButton.animate({
-                opacity: "1.0"
-            }, 200);
-            whichButton.animate({
-                opacity: "0.6"
-            }, 200);
-            userString.push(whichButton);
-            var lastUserIndex = userString.length-1
-            // compare input to cpu string
-            // if false, clear cpu string and user string
-            if (userString[lastUserIndex] != cpuString[lastUserIndex]) {
-                userString = [];
-                cpuString = [];
-                $("#start_text").text("Oops! Game over! Your score was " + score + ".");
-                score=0;
-                updateScore();
-                // change start text on game over
-            // input correct and complete string matches, clear user string for next round and run flashSequence again
-            } else if (userString.length == cpuString.length) {
-                // change start text when round is complete
-                $("#start_text").text("Well done! Prepare for the next round!");
-                score++;
-                updateScore();                
-                userString = [];
-                flashSequence();
-            // otherwise input is correct, but string is not complete, so nothing happens yet
-            } else {
-                $("#start_text").text("So far, so good!");
-            }
+    // make it a function so it only runs when computer finishes
+    // function userInput() {
+        buttons.forEach(function (whichButton,index, array) {
+            whichButton.click(function() {
+                // buttons flash faster for user input
+                whichButton.animate({
+                    opacity: "1.0"
+                }, 200);
+                whichButton.animate({
+                    opacity: "0.6"
+                }, 200);
+                userString.push(whichButton);
+                var lastUserIndex = userString.length-1
+                // compare input to cpu string
+                // if false, clear cpu string and user string
+                if (userString[lastUserIndex] != cpuString[lastUserIndex]) {
+                    userString = [];
+                    cpuString = [];
+                    $("#start_text").text("Oops! Game over! Your score was " + score + ". Press here to try again!").css("font-size", "28px");
+                    score=0;
+                    updateScore();
+                    // change start text on game over
+                // if input correct and complete string matches, clear user string for next round and run flashSequence again
+                } else if (userString.length == cpuString.length) {
+                    // change start text when round is complete
+                    $("#start_text").text("Well done! Prepare for the next round!");
+                    score++;
+                    updateScore();                
+                    userString = [];
+                    flashSequence();
+                // otherwise input is correct, but string is not complete, so nothing happens yet
+                } else {
+                    $("#start_text").text("So far, so good!");
+                    // userInput();
+                }
+            });
         });
-    });
+    // }
 
     // listener for start button runs flash sequence on start
     $("#box_start").click(function() {
